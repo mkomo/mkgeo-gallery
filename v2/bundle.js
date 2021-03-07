ebadc770ef3cddeba15b9e968faa2a9ef7acb078
Matt Komorowski <matt@mkomo.com>
2021-03-07 10:12:16 -0500

    use modules for cleaner scoping

--- v2/bundle.js
+++ v2/bundle.js
@@ -1,117 +1,116 @@
 
 "use strict";
-(() => {
-  const width = window.innerWidth, height = window.innerHeight;
-  const mount = (url, title) => {
-    d3.xml(url).then(data => {
-      const svg = mountSvg(data, title);
-      addTooltip(svg);
-    });
+
+const width = window.innerWidth, height = window.innerHeight;
+const mount = (url, title) => {
+  d3.xml(url).then(data => {
+    const svg = mountSvg(data, title);
+    addTooltip(svg);
+  });
+}
+
+const mountSvg = (data, title='map', container=d3.select('body').append('div')) => {
+  container.append('h1')
+    .style('position', "fixed")
+    .style('top', "10px")
+    .style('left', "10px")
+    .text(title);
+  container.node().append(data.documentElement);
+  const svg = container.select("svg");
+  const g = fitSvg(svg);
+
+  function zoomed({transform}) {
+    g.attr("transform", transform);
   }
 
-  const mountSvg = (data, title='map', container=d3.select('body').append('div')) => {
-    container.append('h1')
-      .style('position', "fixed")
-      .style('top', "10px")
-      .style('left', "10px")
-      .text(title);
-    container.node().append(data.documentElement);
-    const svg = container.select("svg");
-    const g = fitSvg(svg);
+  svg.call(d3.zoom()
+    .extent([[0, 0], [width, height]])
+    .on("zoom", zoomed));
 
-    function zoomed({transform}) {
-      g.attr("transform", transform);
-    }
+  return svg;
+}
 
-    svg.call(d3.zoom()
-      .extent([[0, 0], [width, height]])
-      .on("zoom", zoomed));
+const fitSvg = (svg) => {
+  const g = svg.insert("g").attr("class", "wrapper")
+  svg.selectAll("path").each(function() {
+    var el = this;
+    g.append(function() { return el; });
+  });
 
-    return svg;
-  }
+  svg.attr("preserveAspectRatio", "xMinYMin meet");
+  svg.attr('width', width);
+  svg.attr('height', height);
+  //TODO use to shrink svg down to change svg viewBox: g.node().getBBox();
 
-  const fitSvg = (svg) => {
-    const g = svg.insert("g").attr("class", "wrapper")
-    svg.selectAll("path").each(function() {
-      var el = this;
-      g.append(function() { return el; });
-    });
+  return g;
+}
 
-    svg.attr("preserveAspectRatio", "xMinYMin meet");
-    svg.attr('width', width);
-    svg.attr('height', height);
-    //TODO use to shrink svg down to change svg viewBox: g.node().getBBox();
+/*
+TODO: how viewer should load
+/link/to/viewer
+  ?visible=use-categories.png image to view
+  &overlay=streets.png partially transparent layer that is displayed over the visible layer
+  &bitmap=bitmap.png underlying image that will provide click colors
+  &click=loadPropertyData bitmapClick is a function name that's called with the visible layer color AND optionally the bitmap color that is clicked on
+  &key=use-categories.key.png key to dock on bottom
+*/
 
-    return g;
-  }
-
-  /*
-  TODO: how viewer should load
-  /link/to/viewer
-    ?visible=use-categories.png image to view
-    &overlay=streets.png partially transparent layer that is displayed over the visible layer
-    &bitmap=bitmap.png underlying image that will provide click colors
-    &click=loadPropertyData bitmapClick is a function name that's called with the visible layer color AND optionally the bitmap color that is clicked on
-    &key=use-categories.key.png key to dock on bottom
-  */
-
-  const addTooltip = (svg, container = document.body) => {
-    const tooltip = d3.select(container)
-      .append("div")
-        .style("position", "absolute")
-        .style("visibility", "hidden")
-        .style("user-select", "none")
-        .style("padding", "0.5em")
-        .style("background-color", "rgba(220,220,220,0.5)");
-    const data = {};
-    svg.on("mousemove", function(){
-      const item = event.path[0];
-      if (item.tagName == 'path') {
-        if (data.id != item.id) {
-          extractItemData(item, data)
-          updateTooltip(tooltip, data, event);
-        }
-      } else {
-        if (data.id) {
-          extractItemData(null, data)
-          updateTooltip(tooltip);
-        }
+const addTooltip = (svg, container = document.body) => {
+  const tooltip = d3.select(container)
+    .append("div")
+      .style("position", "absolute")
+      .style("visibility", "hidden")
+      .style("user-select", "none")
+      .style("padding", "0.5em")
+      .style("background-color", "rgba(220,220,220,0.5)");
+  const data = {};
+  svg.on("mousemove", function(){
+    const item = event.path[0];
+    if (item.tagName == 'path') {
+      if (data.id != item.id) {
+        extractItemData(item, data)
+        updateTooltip(tooltip, data, event);
       }
-    });
-  }
-
-  const extractItemData = (item, data) => {
-    data.id = item && item.id;
-    const {x,y,width,height} = item && item.getBBox() || {}
-    Object.assign(data, {x,y,width,height});
-  }
-
-  const TOOLTIP_OFFSET = 28;
-
-  const tooltipData = {
-    visible: false,
-    text: null
-  }
-
-  const updateTooltip = (tooltip, data, event) => {
-    if (data) {
-      tooltip
-        .style("top", (event.pageY + TOOLTIP_OFFSET) + "px")
-        .style("left",(event.pageX + TOOLTIP_OFFSET) + "px")
-      if (!tooltipData.visible) {
-        tooltip.style("visibility", "visible");
-      }
-      const text = Object.values(data || {}).join(',');
-      if (text != tooltipData.text) {
-        tooltip.text(text);
-      }
-      tooltipData.text = text;
-      tooltipData.visible = true;
     } else {
-      tooltip.style("visibility", "hidden");
-      tooltipData.visible = false;
+      if (data.id) {
+        extractItemData(null, data)
+        updateTooltip(tooltip);
+      }
     }
-  }
+  });
+}
 
-  mount("/covid/deaths-09-20.svg", 'deaths as of 9/20');
-})();
+const extractItemData = (item, data) => {
+  data.id = item && item.id;
+  const {x,y,width,height} = item && item.getBBox() || {}
+  Object.assign(data, {x,y,width,height});
+}
+
+const TOOLTIP_OFFSET = 28;
+
+const tooltipData = {
+  visible: false,
+  text: null
+}
+
+const updateTooltip = (tooltip, data, event) => {
+  if (data) {
+    tooltip
+      .style("top", (event.pageY + TOOLTIP_OFFSET) + "px")
+      .style("left",(event.pageX + TOOLTIP_OFFSET) + "px")
+    if (!tooltipData.visible) {
+      tooltip.style("visibility", "visible");
+    }
+    const text = Object.values(data || {}).join(',');
+    if (text != tooltipData.text) {
+      tooltip.text(text);
+    }
+    tooltipData.text = text;
+    tooltipData.visible = true;
+  } else {
+    tooltip.style("visibility", "hidden");
+    tooltipData.visible = false;
+  }
+}
+
+mount("/covid/deaths-09-20.svg", 'deaths as of 9/20');
