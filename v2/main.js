b25e8d6ab400f0158a60f97e28d0fa4e930980d7
Matt Komorowski <matt@mkomo.com>
2021-03-07 22:04:42 -0500

    load bitmap and log out bitmap image data when you click on the visible image

--- v2/main.js
+++ v2/main.js
@@ -3,6 +3,6 @@ import { bitmap } from './modules/bitmap.js'
 
 // mount("/covid/deaths-09-20.svg", 'deaths as of 9/20');
 bitmap({
-  bitmapImage: '../buffalo-properties/bitmap.png',
+  bitmapImageUrl: '../buffalo-properties/bitmap.png',
   visibleImage: '../buffalo-properties/single-family-assessment-per-sqft.png'
 })
--- v2/modules/bitmap.js
+++ v2/modules/bitmap.js
@@ -24,7 +24,7 @@ const addZoomableImageTagToContainer = ({container, imageSrc, pixelated = false,
 }
 
 const addInfoContainer = ({container, title}) => {
-  container.append('div')
+  return container.append('div')
     .text(title)
     .style('position','absolute')
     .style('z-index','10')
@@ -32,11 +32,25 @@ const addInfoContainer = ({container, title}) => {
     .style('left', '10px')
 }
 
+const loadBitmap = bitmapImageUrl => {
+  const canvas = d3.select('body').append('canvas').style('display', 'none'),
+    context = canvas.node().getContext("2d"),
+    bitmapImage = new Image();
+
+  bitmapImage.onload = function() {
+    canvas
+      .attr('width', this.width)
+      .attr('height', this.height);
+    context.drawImage(bitmapImage, 0, 0);
+  };
+  bitmapImage.src = bitmapImageUrl;
+  return canvas;
+}
+
 export const bitmap = ({
-  visibleImage,
-  bitmapImage,
-  visibleBoundingBox,
-  bitmapBoundingBox,
+  visibleImage, // TODO, handle this as a canvas, svg, etc.
+  bitmapImageUrl,
+  bitmapBoundingBox, // TODO treat these as coords that correspond to [[0,0],[width,height]] of visible image
   onHover = ()=>{},
   onClick = ()=>{},
   size = SIZE.CONTAIN,
@@ -47,16 +61,35 @@ export const bitmap = ({
   height = window.innerHeight,
   container = d3.select('body').append('div')
     .style('position','fixed')
-    .style('width', width)
-    .style('height', height),
+    .style('width', width + 'px')
+    .style('height', height + 'px')
+    .style('overflow', 'hidden'),
   bgColor = '#eee'
 }) => {
-  console.log('drawing bitmap', visibleImage, bitmapImage);
+  console.log('drawing image', visibleImage, bitmapImageUrl);
   container
     .style('background-color', bgColor)
     .style('position', 'relative');
 
-  addInfoContainer({container, title});
+  const image = addZoomableImageTagToContainer({container, imageSrc: visibleImage, pixelated: true, width, height})
+  const bitmapCanvas = loadBitmap(bitmapImageUrl);
+  const bitmapContext = bitmapCanvas.node().getContext("2d");
+  const infobox = addInfoContainer({container, title});
 
-  addZoomableImageTagToContainer({container, imageSrc: visibleImage, pixelated: true, width, height})
+
+  image.on('click', function(event) {
+    // const bitmapWidth
+    const imageWidth = image.node().naturalWidth,
+      imageHeight = image.node().naturalHeight,
+      bitmapWidth = bitmapCanvas.attr('width'),
+      bitmapHeight = bitmapCanvas.attr('height'),
+      scaling = bitmapHeight / imageHeight;
+    if (Math.abs(scaling - bitmapHeight/imageHeight) > Number.EPSILON) {
+      console.error('bitmap does not match image aspect ratio. image:', imageWidth, imageHeight,'bitmap:', bitmapWidth, bitmapHeight);
+      //TODO error out
+    }
+    const bitmapData = bitmapContext.getImageData(event.offsetX * scaling,event.offsetY * scaling,1,1).data;
+    console.log('click!', ['offsetX', 'offsetY'].map(key=>event[key]), bitmapData);
+
+  });
 }
